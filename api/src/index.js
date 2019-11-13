c103639669e7c0c863d2dbad550830b98db926bd
hqureshiGmail <33032654+hqureshiGmail@users.noreply.github.com>
2019-11-13 14:25:00 +0500

    Update index.js

--- api/src/index.js
+++ api/src/index.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const query = require('./controllers/query')
 const invoke = require('./controllers/invoke')
+const enrollAdmin = require('./controllers/enrollAdmin')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Welcome to REST API for landrec Blockchain'))
 
 app.get('/query', (req, res) => query.query(req, res))
 
 app.get('/invoke', (req, res) => invoke.invokecc(req, res))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/enrollAdmin', (req, res) => enrollAdmin.enrollAdmin(req, res))
+
+app.listen(port, () => console.log(`Landrec Blockchain REST API listening on port ${port}!`))
